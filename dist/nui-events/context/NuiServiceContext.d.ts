b2018439c26740bdce14ae68dee5fbc64d323149
J Francisco Rader <franciscorader@gmail.com>
2021-04-06 23:07:38 -0300

    add rimraf and react lint settings

--- dist/nui-events/context/NuiServiceContext.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { IAbortableFetch } from "../providers/NuiProvider";
-export interface NuiServiceContext {
-  resource: string;
-  callbackTimeout: number;
-  send: (e: string, data?: unknown) => Promise<Response>;
-  sendAbortable: (e: string, data: unknown) => IAbortableFetch;
-}
-export declare const NuiServiceContext: import("react").Context<NuiServiceContext>;
--- dist/nui-events/context/NuiServiceContext.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NuiServiceContext = void 0;
-console.warn("@ NuiServiceContext is deprecated, please use NuiContext instead");
-var react_1 = require("react");
-exports.NuiServiceContext = react_1.createContext(null);
